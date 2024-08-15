96e11286d423a037a86d0715e6b3dff5fd76ae46
guillermogm <guillermogm009@gmail.com>
2024-08-15 13:16:45 +0200

    Nav bar

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import './Home.css'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <div className='container'>
+        <div className='row'>
+        <div className='col-sm mt-5'>
+            <div class="card mx-auto">
+              <div class="card-body">
+                This is some text within a card body.
+              </div>
+            </div>
+          </div>
+          <div className='col-sm mt-5'>
+            <div class="card mx-auto">
+              <div class="card-body">
+                This is some text within a card body.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
